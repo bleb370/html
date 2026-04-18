3030b6a8b0a8cea6765db474f9549e0ef4bb82c1
bleb370 <adriannzomo345@gmail.com>
2026-04-18 12:59:54 -0500

    Add files via upload

---
+++ # Pandas for managing datasets.py
@@ -0,0 +1,9 @@
+# Pandas for managing datasets
+import pandas as pd
+# Matplotlib for additional customization
+from matplotlib import pyplot as plt
+# Seaborn for plotting and styling
+import seaborn as sb
+# Seaborn for plotting and styling
+df = sb.load_dataset('tips')
+print(df.head())
