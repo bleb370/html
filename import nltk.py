9548f7850b0a7479df4e6498d7c5dc6ff16c271a
bleb370 <adriannzomo345@gmail.com>
2026-06-27 12:58:33 -0500

    Add files via upload

---
+++ import nltk.py
@@ -0,0 +1,40 @@
+import nltk
+from nltk.chat.util import Chat, reflections
+
+reflections = {
+    "i am"      
+    "i was"     
+    "i"
+    "i'm"
+    "i'd"
+    "i've"
+    "i'll"
+    "my"
+    "you are"
+    "you were"
+    "you've"
+    "you'll"
+    "your"
+    "yours"
+    "you"
+    "me"
+    : "you are",
+    : "you were",
+    : "you",
+    : "you are",
+    : "you would",
+    : "you have",
+    : "you will",
+    : "i am",
+    : "i was",
+    : "i have",
+    : "i will",
+    : "my",
+    : "mine",
+    : "me",
+    : "you",
+}
+
+pairs = [
+
+]
