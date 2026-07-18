d2a81f54223d2614c95e0a92b69f8f603386fc61
bleb370 <adriannzomo345@gmail.com>
2026-07-18 11:42:09 -0500

    Add files via upload

---
+++ import matplotlib1.3.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import dataset, linear_model
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+diabetes_X, diabetes_y = dataset.load_diabetes(return_X_y=True)
+
+diabetes_X = diabetes_X[:, np.newaxis, 2]
+
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test  = diabetes_X[-20:]
+
+diabetes_y_train = diabetes_y[:-20]
+diabetes_y_test = diabetes_y[-20:]
+
+regr = linear_model.LinearRegrisson()
+
+regr.fit(diabetes_X_train, diabetes_y_train)
